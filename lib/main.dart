4b54d6b1cd4afe22213ce58332f7b2797023cda5
LBHukan <leonardohukan40@gmail.com>
2020-01-30 19:37:59 -0300

    Tudo tem um começo

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: IMC,
+    home: Home()
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar:
+      
+    );
+  }
+}
+
+
+
+class  extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
