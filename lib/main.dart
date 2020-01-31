8523f9eb645fb880925592d9211828a117961d46
LBHukan <leonardohukan40@gmail.com>
2020-01-30 22:03:01 -0300

    Adicionado taxa do peso

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,21 @@ class _HomeState extends State<Home> {
   void _calculate(){
     setState((){
       double peso = double.parse(pesoController.text);
-    double altura = double.parse(alturaController.text);
+    double altura = double.parse(alturaController.text)/100;
     double imc = peso / (altura * altura);
-    
+         
     if(imc < 18.6){
-      _infotext = "Abaixo do Peso";
+      _infotext = "Abaixo do Peso(${imc.toStringAsPrecision(2)})";
+    }else if(imc >= 18.6 && imc < 24.9){
+      _infotext = "Peso Ideal(${imc.toStringAsPrecision(2)})";
+    }else if(imc >= 24.9 && imc < 29.9){
+      _infotext = "Levemente Acima do Peso(${imc.toStringAsPrecision(2)})";
+    }else if(imc >= 29.9 && imc < 34.9){
+      _infotext = "Obesidade Grau I(${imc.toStringAsPrecision(2)})";
+    }else if(imc >= 34.9 && imc < 39.9){
+      _infotext = "Obesidade Grau II(${imc.toStringAsPrecision(2)})";
+    }else if(imc >= 40){
+      _infotext = "Obesidade Grau III(${imc.toStringAsPrecision(2)})";
     }
     });
   }
